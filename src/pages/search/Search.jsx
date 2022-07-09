36be73ed18a9b2da1ad4858837177d28ccb2c856
armand WADJI <armand_wadji@yahoo.fr>
2022-07-09 03:44:43 +0200

    style in searchComponent

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -3,7 +3,6 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  Dimensions,
   Image,
   Text,
 } from "react-native";
@@ -18,9 +17,6 @@ const Search = () => {
   const getData = (data) => {
     setImage(data);
   };
-
-  const windowWidth = Dimensions.get("window").width;
-  const windowHeight = Dimensions.get("window").height;
   return (
     <SafeAreaView>
       <View style={styles.container}>
@@ -29,51 +25,15 @@ const Search = () => {
           <SearchContent getData={getData} />
         </ScrollView>
         {image && (
-          <View
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              zIndex: 1,
-              backgroundColor: "rgba(52,52,52,0.8)",
-            }}>
+          <View style={styles.modalContainer}>
             <StatusBar backgroundColor='#525252' barStyle='dark-content' />
-            <View
-              style={{
-                position: "absolute",
-                top: windowHeight / 6,
-                left: windowWidth / 10,
-                backgroundColor: "white",
-                width: 350,
-                height: 465,
-                borderRadius: 15,
-                zIndex: 1,
-                elevation: 50,
-              }}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingVertical: 10,
-                  paddingHorizontal: 15,
-                }}>
-                <Image
-                  source={{ uri: image }}
-                  style={{
-                    width: 30,
-                    height: 30,
-                    borderRadius: 100,
-                  }}
-                />
-                <View style={{ paddingLeft: 8 }}>
-                  <Text style={{ fontSize: 12, fontWeight: "600" }}>
-                    Nom de l'utilisateur
-                  </Text>
-                </View>
+            <View style={styles.modal}>
+              <View style={styles.modalHeader}>
+                <Image source={{ uri: image }} style={styles.imageHeader} />
+
+                <Text style={styles.nameHeader}>Nom de l'utilisateur</Text>
               </View>
-              <Image
-                source={{ uri: image }}
-                style={{ width: "100%", height: "80%" }}></Image>
+              <Image source={{ uri: image }} style={styles.bodyHeader}></Image>
             </View>
           </View>
         )}
